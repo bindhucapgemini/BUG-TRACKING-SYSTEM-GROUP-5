bf3dc1a00e2d4ca8de3d26ef40f9706ec364326c
bindhucapgemini <118974316+bindhucapgemini@users.noreply.github.com>
2022-12-28 11:43:56 +0530

    Update choice_def.c

--- GROUP 5 STRUCTURE/09Aug22_SysCPPProgramming_SPRINT01_GROUP05/CUT/Code/src/choice_def.c
+++ GROUP 5 STRUCTURE/09Aug22_SysCPPProgramming_SPRINT01_GROUP05/CUT/Code/src/choice_def.c
@@ -1,17 +1,13 @@
-//Definition file for Main Menu
-
-//this file contains the function defination of the choices we will display in the main menu
-
 #include"login_header.h"
 #include"project_header.h"
 #include"ticket_header.h"
-#include"reports_header.h"
+#include"report_header.h"
 
 
 //function defination for displaying the main menu
-void choiceMain() 
+void choiceMain()
 {
-	int choice;
+        int choice;
 
         printf("\nEnter the choice::\n1.Register\n2.Login\n");
         scanf("%d",&choice);
@@ -25,50 +21,48 @@ void choiceMain()
         {
                 login();
         }
-	mainMenu();
+
 
 }
 
 void mainMenu(void)
 {
-	int option;
-	 			//step1::display menu
-    	printf("\n---Menu---");
-   	printf("\n1.One\
+        int option;
+                                //step1::display menu
+        printf("\n---Menu---");
+        printf("\n1.One\
             \n2.Two\
             \n3.Three\
             \n4.Four");
             printf("\nEnter your option::"); //Step2:get option from user
             scanf("%d",&option);
-    	switch(choice)
-    	{
-        	case 1:
-            		printf("\nManage Projects \n");
-			manageprojects();
-            		break;
-        	case 2:
-            		printf("\nManage Tickets \n");
-			managetickets();
-            		break;
-        	case 3:
-            		printf("\nManage Reports \n");
-			managereports();
-            		break;
-        	case 4:
-            		printf("\n EXIT \n");
-            		break;
-    		default:
-        		printf("\nWrong option");
-    	}//end_switch
-    	printf("\n\outside switch\n");
-    	return 0;
+        switch(option)
+        {
+			                case 1:
+                        printf("\nManage Projects \n");
+                        manage_projects();
+                        break;
+                case 2:
+                        printf("\nManage Tickets \n");
+                        manage_tickets();
+                        break;
+                case 3:
+                        printf("\nManage Reports \n");
+                        manage_reports();
+                        break;
+                case 4:
+                        printf("\n EXIT \n");
+                        break;
+                default:
+                        printf("\nWrong option");
+        }//end_switch
 }
 
 //function definition for displaying menu of manage projects
-void manageprojects()
+void manage_projects()
 {
-	
-        int choice;
+
+        char choice;
         printf("\t---------MANAGE PROJECT-------");
         printf("\n'a': ADD PROJECT\
                 \n'd': DELETE PROJECT\
@@ -77,14 +71,14 @@ void manageprojects()
         printf("\nEnter your choice: ");
         scanf("%s",&choice);
 
- 
+
 
         switch(choice)
         {
                 case ADD:
                         addProject();
                         break;
-                case DELETE:
+			                case DELETE:
                         deleteProject();
                         break;
                 case MODIFY:
@@ -99,90 +93,69 @@ void manageprojects()
 
 //function definition for displaying menu for manage tickets
 
-void managetickets()
+void manage_tickets()
 {
-        int n, i=1;
-        int id = 0;                  //ID intialised to 0
-        printf("BUG TRACKING SYSTEM\n");
-        while (1)
-        {
- 
-                ("Enter choice\n");
-                printf("1.ADDTICKET\n");
-                printf("2.DISPLAYTICKET\n");
-                printf("3.FILE A NEW BUG\n");
-                printf("4.CHANGE THE STATUS OF THE BUG\n");
-                printf("5.Get BUG REPORT\n");
-                printf("6.Close Ticket\n");
-                printf("0.EXIT\n");
-                printf("ENTER YOUR CHOICE:\n");
-                scanf("%d",&n);
-                switch(n) {
-                        case ADDTICKET:
-                                addticket();
-                                break;
-                        case DISPLAYTICKET:
-                                displayticket();
-                                break;
-                        case FILEBUG:
-                                id++;
-                                filebug(id);
-                                break;
-                        case CHANGESTATUS:
-                                changestatus();
-                                break;
-                        case REPORT:
-                                report();
-                                break;
-  			case CLOSETICKET:
-                                closeticket();
-                                break;
-                        case EXIT:
-                                exit(0);
-                                break;
-                        default:
-                                printf("\ninvalid entry");
-                                break;
-                }
-        }
+        int n, i=1;
+        int id = 0;                  //ID intialised to 0
+        printf("BUG TRACKING SYSTEM\n");
+        while (1)
+        {
+
+                ("Enter choice\n");
+                printf("1.ADDTICKET\n");
+                printf("2.DISPLAYTICKET\n");
+                printf("3.FILE A NEW BUG\n");
+                printf("4.CHANGE THE STATUS OF THE BUG\n");
+                printf("5.Get BUG REPORT\n");
+                printf("6.Close Ticket\n");
+                printf("0.EXIT\n");
+                printf("ENTER YOUR CHOICE:\n");
+                scanf("%d",&n);
+                switch(n) {
+                        case ADDTICKET :
+                                         addticket();
+                                         break;
+                        case DISPLAYTICKET :
+                                            displayticket();
+                                            break;
+				                        case FILEBUG :
+                                         id++;
+                                         filebug(id);
+                                         break;
+                        case CHANGESTATUS :
+                                          changestatus();
+                                          break;
+                        case REPORT :
+                                          report();
+                                          break;
+                        case CLOSETICKET :
+                                          closeticket();
+                                          break;
+                        case EXIT :
+                                         exit(0);
+                                         break;
+                        default :
+                                printf("\ninvalid entry");
+                                break;
+                }
+        }
 }
 
-void managereports()
+void manage_reports()
 {
-	char *eid = "123456";
-        printf("(i)Prioritize list of reports not closed\nii)Prioritize list of reports assigned to logged in employee\niii)Prioritize list of reports created by logged in employee \n iv)quit\n");
-        int j;
+    char *eid;
+
+    printf("(i)Prioritize list of reports not closed\nii)Prioritize list of reports assigned to logged in employee\niii)Prioritize list of reports created by logged in employee \n iv)quit\n");
+        int j ;
         scanf("%d",&j);
-
- 
-
         switch(j){
-
- 
-
-                case STATUS : openReports();
-                         break;
-
- 
-
-
-                case ASSIGNED : assigned(eid);
-                         break;
-
- 
-
-
-                case CREATED : created(eid);
-                         break;
-
- 
-
-
-                default : printf("Quit");
-
- 
-
-        return 0;
-        }
+            case 1 : openReports();
+                     break;
+            case 2 : assigned(eid);
+                     break;
+            case 3 : created(eid);
+                     break;
+            default : printf("Quitted");
+	   }
 }
 
